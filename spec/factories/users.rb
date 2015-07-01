449e4e3ad0da5e37590c0c9764ba5d0eb6819410
tgaff <tgaff@alumni.nd.edu>
2015-07-01 15:04:51 -0700

    configure mailer host and add factory and basic user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    email "g@g.com"
+    password "123123456"
+    password_confirmation { "123123456" }
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    subject(:user) { FactoryGirl.create(:user) }
+
+    it { should respond_to(:email) }
+    it { should respond_to(:password) }
+
+    it { should be_valid }
 end
