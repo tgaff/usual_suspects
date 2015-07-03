c8f4cf7c04d8b32da8a62d2cd0950a3c3891507a
tgaff <tgaff@alumni.nd.edu>
2015-07-03 16:33:27 -0700

    Split renamer functionality out into a model and make it much easier to test

--- lib/tasks/usual_suspects.rake
+++ lib/tasks/usual_suspects.rake
@@ -1,53 +1,15 @@
 require 'rspec/core/rake_task'
 require 'tempfile'
 require 'fileutils'
-namespace :usual_suspects do
+require 'modules/usual_suspects/project_renamer'
 
+namespace :usual_suspects do
   desc 'rename the application from UsualSupects to a specified camel-cased new name'
   task :rename_application, :new_name do |t, args|
+    include ProjectRenamer
     new_name = args[:new_name]
-    raise ArgumentError, "Specify a new name for the application!" if new_name.nil?
+    raise ArgumentError, "Specify a new name for the application!" if (new_name.nil? or new_name.empty?)
     
-    list_of_files.each do |fname|
-      find_and_replace_in_file(fname, 'UsualSuspects', new_name)
-      find_and_replace_in_file(fname, underscore('UsualSuspects'), underscore(new_name))
-    end
-  end
-
-  def list_of_files 
-  [
-    'config/application.rb',
-    'app/views/layouts/application.html.erb',
-    'config/initializers/session_store.rb'
-  ]
-  end
-
-  def find_and_replace_in_file(fname, find, replace)
-    file_path = File.expand_path fname
-    temp_file = Tempfile.new('usual_suspects')
-    File.open(file_path, 'r').each do |line|
-      temp_file.puts substitute(line, find, replace)
-    end
-    temp_file.close
-    replace_file(temp_file.path, file_path)
-  end
-
-  def substitute(input, find, replace)
-    input.gsub(find, replace)
-  end
-
-  def replace_file(source_path, dest_path)
-    FileUtils.mv(source_path, dest_path, force: true)
-  end
-
-  private
-
-  #borrowed from ActiveSupport
-  def underscore(str)
-    str.gsub(/::/, '/').
-    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-    gsub(/([a-z\d])([A-Z])/,'\1_\2').
-    tr("-", "_").
-    downcase
+    rename_application(new_name)
   end
 end
--- spec/lib/tasks/usual_suspects_spec.rb
+++ spec/lib/tasks/usual_suspects_spec.rb
@@ -2,13 +2,9 @@ require 'spec_helper'
 require 'rake'
 require 'byebug'
 require 'tempfile'
+require 'pry'
 
 RSpec.describe 'UsualSuspects' do
-  before(:all) do
-    unless git_is_clean?
-      raise 'you must clean or commit all changes before running the renamer tests'
-    end
-  end
 
   before(:each) do
     @rake = Rake::Application.new
@@ -21,61 +17,8 @@ RSpec.describe 'UsualSuspects' do
     #Rake.application.rake_require '../lib/tasks/usual_suspects'
   end
 
- after(:each) do
-   Rake.application = nil
- end
-
- describe 'rake task methods' do
-    describe '.substitute' do
-      let(:initial_text) { "this has an xx but should have a yy" }
-      let(:expected_text) { "this has an yy but should have a yy" }
-
-      it 'returns text replacing one string with another' do
-        expect( substitute(initial_text, 'xx', 'yy')).to eq expected_text
-      end
-    end
-
-    describe '.find_and_replace_in_file' do
-      let(:temp_file) { Tempfile.new('asdf') }
-      before do
-        15.times { temp_file.puts '12341-234 o' }
-        temp_file.close(unlink_now=false)
-      end
-
-      it 'replaces the text in a file' do
-        file_path = temp_file.path
-        find_and_replace_in_file(file_path, '1', 'o')
-        File.open(file_path).each do |line|
-          expect(line).to eq "o234o-234 o\n" # warning, this can pass if the file is empty
-        end
-      end
-
-      it "doesn't add stray bytes" do # test to counter above warning
-        expect{ find_and_replace_in_file(temp_file.path, '3', '4')}.to_not change {temp_file.size}
-      end
-
-      after do
-        temp_file.unlink
-      end
-    end
-
-    describe '.replace_file' do
-      let(:original_file) { Tempfile.new('original') }
-      let(:replacement_file) { Tempfile.new('replacement') }
-
-      before do
-        original_file.write 'all original'
-        replacement_file.write 'replace with me'
-        original_file.close false
-        replacement_file.close false
-      end
-
-      it 'overwrites file x with file y' do
-        replace_file(replacement_file.path, original_file.path)
-
-        expect(File.read(original_file.path)).to eq 'replace with me'
-      end
-    end
+  after(:each) do
+    Rake.application = nil
   end
 
   describe 'tasks' do
@@ -87,37 +30,12 @@ RSpec.describe 'UsualSuspects' do
           expect{task.invoke}.to raise_error ArgumentError
         end
       end
-
-      let(:new_name) { 'TootinPutin' }
-
-      before(:each) do
-        task.invoke(new_name)
+      it 'calls ProjectRenamer.rename_application' do
+        expect_any_instance_of(ProjectRenamer).to receive(:rename_application).and_return(true)
+        task.invoke('BobsBurgers')
       end
-      it 'changes the title in application.html.erb' do
-        fname = 'app/views/layouts/application.html.erb'
-        expect(exists_in_file?(fname, 'UsualSuspects')).to be_falsey
-      end
-
-      it 'changes the key in the session store' do
-        fname = 'config/initializers/session_store.rb'
-        expect(exists_in_file?(fname, 'usual_suspects')).to be_falsey
-        expect(exists_in_file?(fname, 'tootin_putin')).to be true
-      end
-
-      it 'changes the title in application.rb' do
-        fname = 'config/application.rb'
-        expect(exists_in_file?(fname, 'UsualSuspects')).to be_falsey
-        expect(exists_in_file?(fname, new_name)).to be true
-      end
-    end
-
-    def exists_in_file?(fname, string)
-      raise unless File.exists? fname
-      File.open(fname).grep(/#{string}/).length > 0
     end
   end
 end
 
-  def git_is_clean?
-    `git status --porcelain`.empty? ? true : false
-  end
+
